fc06e406c24e5c857c671befb297474fcae72642
Sekhar <sekhar.pasem@wenable.com>
2017-01-10 12:22:38 +0530

    first commit

---
+++ SampleSpring/src/main/java/com/expenner/controller/Person.java
@@ -0,0 +1,42 @@
+package com.expenner.controller;
+
+public class Person {
+	private String name;
+	private String username;
+
+	public Person(String name, String username) {
+		this.name = name;
+		this.username = username;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username
+	 *            the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
